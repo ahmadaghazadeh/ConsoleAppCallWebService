77e25710c614ff229f2f8576227ecc6d3cba8bf3
Ahmad Aghazadeh <ahmad.aghazadeh.a@gmail.com>
2016-02-10 13:06:57 +0330

    First commit

---
+++ src/ConsoleAppCallWebService/Module1.vb
@@ -0,0 +1,71 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Try
+            Dim reqparm As New Specialized.NameValueCollection
+            Dim pram As String()
+            Dim url As String = ""
+            Dim message As String = ""
+
+            For Each argument As String In My.Application.CommandLineArgs
+                pram = argument.Split("=")
+                If (pram.Length = 2) Then
+                    reqparm.Add(pram(0), pram(1))
+
+                Else
+                    url = argument
+                End If
+
+            Next
+
+
+
+            If (reqparm.Count > 1) Then
+                Using client As New Net.WebClient
+
+                    Dim responsebytes = client.UploadValues(url, "POST", reqparm)
+
+                    Dim responsebody = (New Text.UTF8Encoding).GetString(responsebytes)
+
+                    Dim strFile As String = "response.txt"
+                    Dim fileExists As Boolean = File.Exists(strFile)
+                    If (Not fileExists) Then
+                        Using sw As New StreamWriter(File.Open(strFile, FileMode.OpenOrCreate))
+                            sw.WriteLine(DateTime.Now & " " & message & " " & responsebody)
+                            Console.Write(responsebody + vbCrLf + message)
+                        End Using
+                    Else
+                        Using sw As New StreamWriter(File.Open(strFile, FileMode.Append))
+                            sw.WriteLine(DateTime.Now & " " & message & " " & responsebody)
+                            Console.Write(responsebody + vbCrLf + message)
+                        End Using
+                    End If
+
+                    Console.Write("Complete Post to url")
+
+                End Using
+            End If
+        Catch ex As Exception
+            Dim strFile As String = "error.txt"
+            Dim fileExists As Boolean = File.Exists(strFile)
+            If (Not fileExists) Then
+                Using sw As New StreamWriter(File.Open(strFile, FileMode.OpenOrCreate))
+                    sw.WriteLine(DateTime.Now & ex.Message)
+                End Using
+            Else
+                Using sw As New StreamWriter(File.Open(strFile, FileMode.Append))
+                    sw.WriteLine(DateTime.Now & ex.Message)
+                End Using
+            End If
+            Console.Write("Error : " + ex.Message)
+
+        End Try
+
+
+
+
+    End Sub
+
+End Module
---
+++ src/ConsoleAppCallWebService/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleAppCallWebService")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleAppCallWebService")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c16ec6de-6111-4687-bd57-4b05eb26cd9e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
